5845fbe337feefd5ec87b5eff3d7f42b2ec8f49c
Robin Soni <sonirobin369@gmail.com>
2024-06-15 17:23:20 +0530

    cards section added

--- front-page.php
+++ front-page.php
@@ -1,13 +1,16 @@
 <?php get_header(); ?>
-<?php
-$image_bg_url =  get_template_directory_uri()."/assets/img/mahabharat.png";
-?>
-<section class="hero-section site-container"  >
-    <div class="image-container flex w-full h-[500px]  hero bg-custom-image bg-no-repeat bg-cover bg-[center_center] rounded-md">
-        <!-- <img class="bg-cover h-full w-full object-cover object-center  " src="<?php echo get_template_directory_uri()."/assets/img/mahabharat.png"; ?>" alt="" srcset="" /> -->
+<section class="hero-section site-container">
+    <div class="image-container flex w-full h-[500px]  hero bg-custom-image bg-no-repeat bg-cover bg-[center_center] rounded-md"> 
     </div>
 </section>
-<section class="ad-section h-[160px] w-full ">   
+<section class="ad-section h-[160px] w-full ">
 </section>
-
+<section class="post-container site-container grid grid-cols-3 gap-3">
+    <?php
+    $count = 7;
+    while($count-- >0){
+        get_template_part( 'parts/content', 'card' );
+        }    
+    ?>  
+</section>
 <?php get_footer(); ?>
---
+++ parts/content-card.php
@@ -0,0 +1,16 @@
+<div class="card-container w-full border rounded-md  p-[15px] hover:shadow-2xl cursor-pointer    transition-transform  ease-in-out  delay-75  hover:scale-[1.02]   ">
+        <div class="card">
+            <img class="h-[240px] w-full" src="<?php echo get_template_directory_uri() . "/assets/img/card-img-placeholder.png"; ?>" alt="">
+            <div class="card-details mt-4  mb-4">
+                <span class="inline-flex items-center rounded-md bg-blue-50 px-2 py-1 text-xs font-medium text-blue-700 ring-1 ring-inset ring-blue-700/10">Technology</span>
+                <div class="text-xl  font-semibold mt-2 ">The Impact of Technology on the Workplace: How Technology is Changing</div>
+                <div class="author flex justify-between  items-center  text-gray-500 w-3/4  mt-4  ">
+                    <div class="author-details flex items-center">
+                        <img class="mr-2 " src="<? echo get_template_directory_uri() . "/assets/img/author-image.png"; ?>" alt="">
+                        <span class="author-name">Tracey Wilson</span>
+                    </div>
+                    <span class="posted-on">August 20,2022</span>
+                </div>
+            </div>
+        </div>
+    </div>
